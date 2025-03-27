6ac74478decff79b27b6ebaa159f5c1c99814345
Joel <jponcelas@cendrassos.net>
2025-03-27 19:52:47 +0100

    ....

---
+++ TicTacToe/Partida.cs
@@ -0,0 +1,8 @@
+namespace TicTacToe;
+
+public class Partida
+{
+    public string Tauler { get; set; }
+    public string Jugador1 { get; set; }
+    public string Jugador2 { get; set; }
+}
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -10,6 +10,7 @@ namespace TicTacToe;
 class Program
 {
     private static readonly HttpClient client = new HttpClient { BaseAddress = new Uri("http://localhost:8080/") };
+    private const string JugadorDesqualificada = "Espanya";
 
     static async Task Main(string[] args)
     {
@@ -23,16 +24,35 @@ class Program
 
         string pattern = @"participant (?<nom>[A-Za-z]+ [A-Za-z'-]+).*representa(nt)? (a|de) (?<pais>[A-za-z]+)";
 
-        foreach (var participant in participants)
+        foreach (var participant1 in participants)
         {
-            Match match = Regex.Match(participant, pattern);
+            Match match = Regex.Match(participant1, pattern);
             if (match.Success)
             {
                 string nom = match.Groups["nom"].Value;
                 string cognom = match.Groups["cognom"].Value;
                 string pais = match.Groups["pais"].Value;
-                Console.WriteLine($"Nom: {nom} {cognom}, País: {pais}");
+                
+                if (pais != JugadorDesqualificada)
+                {
+                    Console.WriteLine($"Nom: {nom} {cognom}, País: {pais}");
+
+                }
             }
         }
     }
+
+    private static string DeterminarGuanyador(string tauler)
+    {
+        string[] liniesGuanyadores = { "012", "345", "678", "036", "147", "258", "048", "246" };
+
+        foreach (var line in liniesGuanyadores)
+        {
+            
+        }
+    }
+    
+    
+    
+    
 }
